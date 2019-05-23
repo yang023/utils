25a45c426c360aa550172b8e49cfb0e17ca9a4b1
yang023 <zjy799281327@qq.com>
2019-05-23 14:20:13 +0800

    删除无必要的方法

--- src/main/java/cn/yang/util/core/BasicUtil.java
+++ src/main/java/cn/yang/util/core/BasicUtil.java
@@ -25,7 +25,7 @@ public class BasicUtil {
     /**
      * 生成10位时间戳
      *
-     * @param time   生成时间戳的具体时间，null则为当前时间
+     * @param time 生成时间戳的具体时间，null则为当前时间
      * @return 时间戳
      */
     public static Long timestamp(LocalDateTime time) {
@@ -35,23 +35,24 @@ public class BasicUtil {
         return time.atZone(ZoneId.systemDefault()).toEpochSecond();
     }
 
+    /**
+     * 获取当前时间的时间戳
+     *
+     * @return long类型时间戳
+     */
     public static Long timestamp() {
         return timestamp(null);
     }
 
+    /**
+     * 根据时间戳转译出时间
+     *
+     * @param timestamp 待转换的时间戳
+     * @return 指定时间
+     */
     public static LocalDateTime fromTimestamp(long timestamp) {
-        return LocalDateTime.ofEpochSecond(timestamp, 0, ZoneOffset.ofHours(8));
-    }
-
-    public static LocalDateTime now(ZoneId zoneId) {
-        if (Objects.isNull(zoneId)) {
-            zoneId = ZoneId.systemDefault();
-        }
-        return LocalDateTime.now(zoneId);
-    }
-
-    public static LocalDateTime now() {
-        return now(null);
+        return LocalDateTime.ofEpochSecond(timestamp, 0,
+                ZoneOffset.ofHours(DEFAULT_OFFSET_HOURS));
     }
 
 }
